d1a7c25a155f8691d5f7762418667e1ef5843f25
Numostanley <nanateinumonde@gmail.com>
2024-01-16 16:11:57 +0100

    Updated authentication

--- utils/authentication.go
+++ utils/authentication.go
@@ -165,3 +165,47 @@ func (tAuth *TokenAuthentication) Authenticate(request *http.Request) (map[strin
 
 	return decodedToken, user, nil
 }
+
+func PerformAuthentication(clientID, clientSecret, grantType, email, password, scope string) (*models.Client, *models.User, error) {
+	client, err := GetClientByClientID(clientID)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("invalid_client %s", err)
+	}
+
+	if !client.ValidateSecret(clientSecret) {
+		return nil, nil, fmt.Errorf("invalid_client_credentials")
+	}
+
+	if !client.ValidateGrantType(grantType) {
+		return nil, nil, fmt.Errorf("invalid_grant_type")
+	}
+
+	if !client.ValidateScope(scope) {
+		return nil, nil, fmt.Errorf("invalid_scope")
+	}
+
+	user, err := GetUserByEmail(email)
+
+	if err != nil {
+		return client, nil, fmt.Errorf("invalid_user %s", err)
+	}
+
+	// if !user.IsEmailVerified {
+	// 	return client, nil, fmt.Errorf("invalid_user")
+	// }
+
+	if !user.ValidatePassword(password) {
+		return client, nil, fmt.Errorf("invalid_user_credentials")
+	}
+
+	if !user.ValidateUserAgainstClientID(clientID) {
+		return client, nil, fmt.Errorf("invalid_client_and_user")
+	}
+
+	// if user.HasActiveSession() {
+	// 	return client, nil, fmt.Errorf("user_has_an_active_session")
+	// }
+
+	return client, user, nil
+}
